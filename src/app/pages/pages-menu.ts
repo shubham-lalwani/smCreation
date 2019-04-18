e0387c2d355813461039289af437da78d3df7887
Shubham Lalwani <shubhamlalwani88@gmail.com>
2019-04-18 18:20:29 +0530

    initial commit

---
+++ src/app/pages/pages-menu.ts
@@ -0,0 +1,90 @@
+import { NbMenuItem } from '@nebular/theme';
+
+export const MENU_ITEMS: NbMenuItem[] = [
+  {
+    title: 'Dashboard',
+    icon: 'nb-e-commerce',
+    link: '/pages/dashboard',
+    home: true,
+  },  
+  {
+    title: 'FEATURES',
+    group: true,
+  },  
+  {
+    title: 'Charts',
+    icon: 'nb-bar-chart',
+    children: [
+      {
+        title: 'Echarts',
+        link: '/pages/charts/echarts',
+      },
+      {
+        title: 'Charts.js',
+        link: '/pages/charts/chartjs',
+      },
+      {
+        title: 'D3',
+        link: '/pages/charts/d3',
+      },
+    ],
+  },
+  {
+    title: 'Consult Fitness Experts',
+    icon: 'nb-bar-chart',
+    children: [
+      {
+        title: 'Physiotherapist',
+        link: '',
+      },
+      {
+        title: 'Dietician',
+        link: '',
+      },
+    ],
+  },
+  {
+    title: 'Fitness Partners',
+    icon: 'nb-bar-chart',
+    children: [
+      {
+        title: 'Golds Gym',
+        link: '',
+      },
+      {
+        title: 'Signature',
+        link: '',
+      },
+      {
+        title:'Fitternity',
+        link:''
+      }
+    ],
+  },
+  {
+    title: 'Diet Chart',
+    icon: 'nb-bar-chart',
+    link:''
+  },
+  {
+    title: 'Membership',
+    icon: 'nb-bar-chart',
+    link:''
+  },
+  {
+    title: 'FAQ',
+    icon: 'nb-bar-chart',
+    link:''
+  },
+  {
+    title: 'Contact US',
+    icon: 'nb-bar-chart',
+    link:''
+  },
+  {
+    title: 'Careers',
+    icon: 'nb-bar-chart',
+    link:''
+  },
+
+];
---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+
+import { PagesComponent } from './pages.component';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { ECommerceModule } from './e-commerce/e-commerce.module';
+import { PagesRoutingModule } from './pages-routing.module';
+import { ThemeModule } from '../@theme/theme.module';
+
+const PAGES_COMPONENTS = [
+  PagesComponent,
+];
+
+@NgModule({
+  imports: [
+    PagesRoutingModule,
+    ThemeModule,
+    DashboardModule,
+    ECommerceModule,
+  ],
+  declarations: [
+    ...PAGES_COMPONENTS,
+  ],
+})
+export class PagesModule {
+}
